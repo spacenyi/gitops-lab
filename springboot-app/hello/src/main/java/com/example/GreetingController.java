c0d632e55e72f42494f367e2d49486a34941ff81
spacenyi <spacenyi@naver.com>
2026-05-24 14:47:09 +0900

    마지막

--- springboot-app/hello/src/main/java/com/example/GreetingController.java
+++ springboot-app/hello/src/main/java/com/example/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s! ^..^OTL";
+	private static final String template = "Hello, %s! ㅠ.ㅠOTL^..^";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/hello")
